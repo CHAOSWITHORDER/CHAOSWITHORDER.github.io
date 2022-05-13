2ffb393876ed10c5652e0fa56aa979158370c2a4
akliuxingyuan <akliuxingyuan@gmail.com>
2022-05-13 16:59:09 +0800

    update: 2022-05-13 16:59:09

--- component---src-pages-404-js-d9768df888d997c8034a.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_mix=self.webpackChunkgatsby_starter_mix||[]).push([[883],{9616:function(t,e,a){"use strict";a.r(e);var n=a(7294),s=a(6438),l=a(3751);e.default=function(t){var e=t.data,a=t.location,r=e.site.siteMetadata.title;return n.createElement(s.Z,{location:a,title:r},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-d9768df888d997c8034a.js.map
